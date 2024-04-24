6f62c2c5f55030035bb4eb4337032d116f04db2b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-04-24 09:12:36 -0400

    chore: chases p6common refactor

--- lib/pyenv.sh
+++ lib/pyenv.sh
@@ -8,7 +8,7 @@
 ######################################################################
 p6df::modules::python::pyenv::latest() {
 
-  pyenv install -l | p6_filter_select '^ *3' | p6_filter_exclude "[a-z]" | p6_filter_last "1" | p6_filter_spaces_strip
+  pyenv install -l | p6_filter_row_select '"^ *3"' | p6_filter_row_exclude "[a-z]" | p6_filter_row_last "1" | p6_filter_spaces_strip
 }
 
 ######################################################################
@@ -20,7 +20,7 @@ p6df::modules::python::pyenv::latest() {
 ######################################################################
 p6df::modules::python::pyenv::latest::installed() {
 
-  pyenv install -l | p6_filter_select '^ *3' | p6_filter_exclude "[a-z]" | p6_filter_from_end "2" | p6_filter_spaces_strip
+  pyenv install -l | p6_filter_row_select '"^ *3"' | p6_filter_row_exclude "[a-z]" | p6_filter_row_from_end "2" | p6_filter_spaces_strip
 }
 
 ######################################################################
