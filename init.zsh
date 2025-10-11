bacba3ae0138418d00101a27bf7f7c1f43083819
Philip M. Gollucci <pgollucci@p6m7g8.com>
2025-10-10 20:16:08 -0400

    feat(mgr): pipenv, etc -> uv

--- init.zsh
+++ init.zsh
@@ -10,10 +10,8 @@ p6df::modules::python::deps() {
   ModuleDeps=(
     p6m7g8-dotfiles/p6df-zsh
     p6m7g8-dotfiles/p6python
-    pyenv/pyenv
+    astral-sh/zsh-uv-env
     ohmyzsh/ohmyzsh:plugins/uv
-    ohmyzsh/ohmyzsh:plugins/poetry
-    ohmyzsh/ohmyzsh:plugins/poetry-env
   )
 }
 
@@ -71,20 +69,6 @@ p6df::modules::python::external::brew() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::python::home::symlink()
-#
-#  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
-#>
-######################################################################
-p6df::modules::python::home::symlink() {
-
-  p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-python/share/.pip" ".pip"
-  p6_return_void
-}
-
-######################################################################
-#<
-#
 # Function: p6df::modules::python::langs()
 #
 #>
@@ -94,7 +78,6 @@ p6df::modules::python::langs() {
   p6df::modules::python::langs::pull
   p6df::modules::python::langs::nuke
   p6df::modules::python::langs::install
-  p6df::modules::python::langs::pip
 
   p6_return_void
 }
@@ -117,54 +100,10 @@ p6df::modules::python::init() {
 
   p6_bootstrap "$dir"
 
-  compdef _pipenv pipenv
-
-  p6_path_if "$HOME/.local/bin" "$PATH" # XXX Must be before shims
-  p6df::core::lang::mgr::init "$P6_DFZ_SRC_DIR/pyenv/pyenv" "py"
+  p6df::core::lang::mgr::init2 "py"
   # eval "$($bin init --path)"
 
   p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: str str = p6df::modules::pipenv::prompt::info()
-#
-#  Returns:
-#	str - str
-#
-#  Environment:	 P6_DFZ_REAL_CMD PIPENV_ACTIVE
-#>
-######################################################################
-p6df::modules::pipenv::prompt::info() {
-
-  local cache_key="pipenv"
-
-  case "$P6_DFZ_REAL_CMD" in
-  *pipenv* | *cd*)
-    grep -v "^$cache_key=" "$P6_DFZ_PROMPT_CACHE" >"$P6_DFZ_PROMPT_CACHE.tmp" && mv "$P6_DFZ_PROMPT_CACHE.tmp" "$P6_DFZ_PROMPT_CACHE"
-    local env=$(p6_run_code pipenv --venv 2>/dev/null)
-    echo "$cache_key=$env" >>"$P6_DFZ_PROMPT_CACHE"
-    ;;
-  esac
-
-  local env=$(grep -E "^$cache_key=" "$P6_DFZ_PROMPT_CACHE" | tail -1 | cut -d '=' -f 2)
-
-  if ! p6_string_blank "$env"; then
-    env=$(p6_uri_name "$env")
-    local astr
-    if p6_string_eq "$PIPENV_ACTIVE" "1"; then
-      astr="active"
-    else
-      astr="off"
-    fi
-
-    local str="pipenv:\t\t  $env ($astr)"
-    p6_return_str "$str"
-  else
-    p6_return_void
-  fi
 }
 
 ######################################################################
@@ -178,11 +117,16 @@ p6df::modules::pipenv::prompt::info() {
 #  Environment:	 PYENV_ROOT PYTHONPATH
 #>
 ######################################################################
-p6df::modules::py::env::prompt::info() {
+p6df::modules::uv::env::prompt::info() {
 
-  local str="pyenv_root:\t  $PYENV_ROOT"
+  local str=""
+
+  local ver=$(uv python pin 2>/dev/null)
+  if ! p6_string_blank "$ver"; then
+    str="uv:\t\t  $ver [$VIRTUAL_ENV_PROMPT] $P6_NL"
+  fi
   if ! p6_string_blank "$PYTHONPATH"; then
-    str=$(p6_string_append "$str" "pythonpath:\t  $PYTHONPATH" "$P6_NL")
+    str="${str}pythonpath:\t  $PYTHONPATH $P6_NL"
   fi
 
   p6_return_str "$str"
